01f1d2cca2878925d71b56ecfd64d246acee4ce2
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-09-14 12:51:53 +0900

    renew

--- chapter01/lesson02/js/script.js
+++ chapter01/lesson02/js/script.js
@@ -1,4 +1,23 @@
+// $(function(){
+// 	$("button").html("Click");
+// 	$("button").css("background","#000033");
+// });
+
+// $(function(){
+// 	alert($("button").html());
+// 	$("button").css("background","#000033");
+// });
+
+// $(function(){
+// 	$("button").click(function(){
+// 		$("button").html("Click");
+// 		$("button").css("background","#000033");
+// 	});
+// });
+
 $(function(){
-	$("button").html("Click");
-	$("button").css("background","#000033");
+	$("button").click(function(){
+		$(this).html("Click");
+		$(this).css("background","#000033");
+	});
 });
