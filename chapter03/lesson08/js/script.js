92bc9672a663f5a1716f9800602e5115a6cc2bec
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-09-15 21:57:30 +0900

    renew

--- chapter03/lesson08/js/script.js
+++ chapter03/lesson08/js/script.js
@@ -1,3 +1,33 @@
-$(function(){
+// $(function(){
+//     // #tab1以外を非表示に
+//     $('#contents div[id !="tab1"]').hide();
 
-});
+//     // タブをクリック
+//     $('a').click(functon(){
+//         $('#contents div').hide();
+//         $(this).attr('href');
+//         return false;
+//     });
+// });
+
+$(function(){
+    // #tab1以外を非表示にする
+    $('#contents div[id != "tab1"]').hide();
+
+    // タブをクリック
+    $("a").click(function(){
+      // 一度全てのコンテンツを非表示にする
+      $("#contents div").hide();
+
+      // 選択されたコンテンツを再表示する
+      $($(this).attr("href")).show();
+
+      // 現在のcurrentクラスを削除
+      $(".current").removeClass("current");
+
+      // 選択されたタブ（自分自身）にcurrentクラスを追加
+      $(this).addClass("current");
+
+      return false;
+    });
+  });
